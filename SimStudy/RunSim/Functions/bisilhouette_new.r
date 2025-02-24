188a36b8c5f79316fbc160fa9a6f937a350f51a9
Ella Orme <76916977+eso28599@users.noreply.github.com>
2025-02-24 08:54:45 +0000

    formatting of bisilhouette finished, changed to standardisation

--- SimStudy/RunSim/Functions/bisilhouette_new.r
+++ SimStudy/RunSim/Functions/bisilhouette_new.r
@@ -1,34 +1,13 @@
-# which of these are actually necessary?
-library(philentropy)
-library(Matrix)
-library(clue)
-library(aricode)
-library(rio)
-library(eList)
-library(foreach)
-library(doParallel)
-library(doSNOW)
-library(MASS)
-
-normalise_matrix <- function(matrix) {
-  #' Normalises a matrix to have column sums of 1.
-  #' Args:
-  #'  matrix: input matrix, shape (N, p).
-  #' Returns:
-  #'  The normalised matrix,  shape (N, p).
-  return(matrix %*% solve(diag(colSums(matrix))))
-}
-
-check_unique <- function(matrix) {
+check_unique <- function(row_clustering) {
   #' Check if there are at least two unique row clusters.
   #' Args:
-  #'  matrix: binary matrix indicating row clustering, shape(N, k).
+  #'  row_clustering: binary matrix indicating row clustering, shape(N, k).
   #' Returns:
   #'  TRUE if there are at least two unique row clusters, bool.
-  if (sum(colSums(matrix) != 0) > 1) {
-    matrix <- matrix[, colSums(matrix) != 0]
+  if (sum(colSums(row_clustering) != 0) > 1) {
+    row_clustering <- row_clustering[, colSums(matrix) != 0]
   }
-  n_clusts <- ncol(matrix)
+  n_clusts <- ncol(row_clustering)
   equal <- diag(n_clusts)
   for (i in 1:(n_clusts - 1)) {
     for (j in (i + 1):n_clusts) {
@@ -40,9 +19,71 @@ check_unique <- function(matrix) {
   return(nrow(unique(equal)) == 2)
 }
 
-inner_bis <- function(data, row_clustering,
-                      col_clustering,
-                      method = "euclidean") {
+calculate_scores <- function(distances, indices, k, n_clusts_row, clust_two) {
+  #' Calculate the a and b values for the bisilhouette score.
+  #' Args:
+  #'  distances: distance (over the columns in question) matrix, shape (N, N).
+  #'  indices: binary vector indicating row cluster, shape (N, ).
+  #'  k: current row cluster index, int.
+  #'  n_clusts_row: number of row clusters, int.
+  #'  clust_two: binary matrix indicating row clustering,
+  #'             shape(N, n_clusts_row).
+  #' Returns:
+  #'  bisil: bisilhouette score, float.
+  b_vec <- c()
+  # calculate a values
+  if (sum(indices) == 1) {
+    # if only one element belongs to row cluster, set a=0
+    a_vals <- 0
+  } else {
+    a_vals <- apply(
+      distances[indices, indices],
+      1, function(x) sum(x) / (length(x) - 1)
+    )
+  }
+  # calculate b values
+  # indices for other clusters
+  other <- (1:n_clusts_row)[-k]
+  b_vals <- vector("list", length = (n_clusts_row - 1))
+  t <- 1
+  # consider every other cluster
+  for (l in other) {
+    oth_ind <- clust_two[, l] == 1
+    if ((sum(oth_ind) == 0) || all(oth_ind == indices)) {
+      # if the other cluster is empty,
+      # or if the other cluster is the same as the current cluster
+      # set b=Inf
+      b_val <- rep(Inf, sum(indices))
+    } else if ((sum(oth_ind) == 1) || (sum(indices) == 1)) {
+      # if either cluster has only one element
+      # need to calculate mean differently
+      b_val <- mean(distances[indices, oth_ind])
+    } else {
+      b_val <- rowMeans(distances[indices, oth_ind])
+    }
+    b_vec <- c(b_vec, mean(b_val))
+    b_vals[[t]] <- b_val
+    t <- t + 1
+  }
+  b_vals <- b_vals[[which.min(b_vec)]]
+  if (all(b_vals == Inf) || (all(b_vals == 0) && all(a_vals == 0))) {
+    # if all b values are Inf, set score to 0
+    # this corresponds to all other clusters being empty
+    # or the same as the current cluster
+    # if all a and b values are 0, set score to 0
+    # this corresponds to the current cluster being empty
+    bis_vals <- 0
+    bisil_score <- 0
+  } else {
+    bis_vals <- (b_vals - a_vals) / apply(rbind(b_vals, a_vals), 2, max)
+    bisil_score <-  mean(bis_vals)
+  }
+  return(list("bisil" = bisil_score, "bis_vals" = bis_vals))
+}
+
+calculate_bis <- function(data, row_clustering,
+                          col_clustering,
+                          method = "euclidean") {
   #' Calculate the bisilhouette score without repeats
   #'
   #' Args:
@@ -76,7 +117,7 @@ inner_bis <- function(data, row_clustering,
   rep <- ifelse(n_clusts_row == n_clusts, FALSE, TRUE)
 
   # calculate score for each cluster
-  s_vals <- vector("list", length = n_clusts)
+  bis_vals <- vector("list", length = n_clusts)
   for (k in 1:n_clusts) {
     indices <- clust_two[, k] == 1
     # if row or col cluster empty, set score to 0
@@ -84,70 +125,21 @@ inner_bis <- function(data, row_clustering,
       bisil_score[k] <- bisil_score[k] + 0
     } else {
       # subset data using column cluster k
-      new_data <- X[, (clust_one[, k] == 1)]
+      new_data <- data[, (clust_one[, k] == 1)]
       distances <- as.matrix(stats::dist(new_data, method))
-      b_vec <- c()
-      # calculate a values
-      if (sum(indices) == 1) {
-        # if only one element belongs to row cluster, set a=0
-        a_vals <- 0
-      } else {
-        a_vals <- apply(
-          distances[indices, indices],
-          1, function(x) sum(x) / (length(x) - 1)
-        )
-      }
-      # calculate b values
-      # indices for other clusters
-      other <- (1:n_clusts_row)[-k]
-      b_vals <- vector("list", length = (n_clusts_row - 1))
-      t <- 1
-      # consider every other cluster
-      for (l in other) {
-        oth_ind <- clust_two[, l] == 1
-        if ((sum(oth_ind) == 0) || all(oth_ind == indices)) {
-          # if the other cluster is empty,
-          # or if the other cluster is the same as the current cluster
-          # set b=Inf
-          b_val <- rep(Inf, sum(indices))
-        } else if ((sum(oth_ind) == 1) || (sum(indices) == 1)) {
-          # if either cluster has only one element
-          # need to calculate mean differently
-          b_val <- mean(distances[indices, oth_ind])
-        } else {
-          b_val <- rowMeans(distances[indices, oth_ind])
-        }
-        b_vec <- c(b_vec, mean(b_val))
-        b_vals[[t]] <- b_val
-        t <- t + 1
-      }
-      b_vals <- b_vals[[which.min(b_vec)]]
-      # calculate bisilhouette score
-      if (all(b_vals == Inf) || (all(b_vals == 0) && all(a_vals == 0))) {
-        # if all b values are Inf, set score to 0
-        # this corresponds to all other clusters being empty
-        # or the same as the current cluster
-        # if all a and b values are 0, set score to 0
-        # this corresponds to the current cluster being empty
-        s_vals[[k]] <- 0
-        bisil_score[k] <- bisil_score[k] + 0
-      } else {
-        s_con <- (b_vals - a_vals) / apply(rbind(b_vals, a_vals), 2, max)
-        s_vals[[k]] <- s_con
-        bisil_score[k] <- bisil_score[k] + mean(s_con)
-      }
+      # calculate scores
+      scores <- calculate_scores(distances, indices, k, n_clusts_row, clust_two)
+      bisil_score[k] <- bisil_score[k] + scores$bisil
+      bis_vals[[k]] <- scores$bis_vals
     }
   }
   # calculate overall score
-  if (sum(bisil_score) == 0) {
-    bisil <- 0
-  } else {
-    bisil <- ifelse(sum(bisil_score != 0) == 1, sum(bisil_score),
-      sum(bisil_score) / (sum(bisil_score != 0))
-      - 2 * sd(bisil_score[bisil_score != 0])
-    )
-  }
-  return(list("bisil" = bisil, "vals" = s_vals, "repeat" = rep))
+  bisil <- ifelse(sum(bisil_score != 0) <= 1,
+    sum(bisil_score),
+    sum(bisil_score) / (sum(bisil_score != 0))
+    - 2 * sd(bisil_score[bisil_score != 0])
+  )
+  return(list("bisil" = bisil, "vals" = bis_vals, "repeat" = rep))
 }
 
 
@@ -167,35 +159,25 @@ bisilhouette <- function(data, row_clustering, col_clustering,
   #'  vals: individual sample scores, shape (N, ).
 
   # Error handling
-  if (nrow(data) != nrow(row_clustering)) {
-    stop("Number of rows in data and row clustering do not match.")
-  }
-  if (ncol(data) != nrow(col_clustering)) {
-    stop("Number of columns in data and column clustering do not match.")
+  if (any(dim(data) != c(nrow(row_clustering), nrow(col_clustering)))) {
+    stop("Dimensions of data and clustering matrices do not match.")
   }
   if (ncol(row_clustering) != ncol(col_clustering)) {
     stop("Number of row and column clusters do not match.")
   }
 
-  # set seed
-  if (!seed) {
-    set.seed(seed)
-  }
-
-  # normalise input data if needed
-  if (any(colSums(data) != 1)) {
-    data <- normalise_matrix(data)
-  }
+  if (!seed) set.seed(seed)
+  data <- scale(data)
 
   # initial results
-  results <- bisilhouette_inner(data, row_clustering, col_clustering, method)
+  results <- calculate_bis(data, row_clustering, col_clustering, method)
   bisil <- results$bisil
   vals <- results$vals
   # repeat if necessary
   if (results$rep) {
     for (i in 1:(n_reps - 1)) {
-      res_rep <- bisilhouette_inner(data, row_clustering,
-                                    col_clustering, method)
+      res_rep <- calculate_bis(data, row_clustering,
+                               col_clustering, method)
       bisil <- bisil + res_rep$bisil
       vals <- lapply(seq_along(length(vals)),
                      function(k) vals[[k]] + res_rep$vals[[k]])
