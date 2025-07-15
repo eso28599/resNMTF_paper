cda6d0aaa15c6be14424901af08de9d2fd8e8af7
Ella Orme <76916977+eso28599@users.noreply.github.com>
2025-07-15 11:38:24 +0100

    combine all dis results

--- RealData/combine_results.r
+++ RealData/combine_results.r
@@ -7,7 +7,7 @@ cancers <- read.csv("RealData/3cancers/distance_study.csv")
 
 # table with f score results for different distances
 # used with bisilhouette score
-all_dis <- t(rbind(sources, bbc, single_cell, cancers)[, 2:17])
+all_dis <- t(rbind(sources, bbc, single_cell, cancers)[, 2:13])
 all_dis <- cbind(
   rep(c("Euclidean", "Cosine", "Manhattan"), each = 4),
   rep(c("E", "C", "M", "F"), 3),
@@ -15,8 +15,11 @@ all_dis <- cbind(
 )
 all_dis <- data.frame(all_dis)
 all_dis[, 3:11] <- apply(all_dis[, 3:11], 2, as.numeric)
-colnames(all_dis) <- c("", "", rep(c("3sources", "BBCSport", "A549", "3cancers"), each = 3))
-write.csv(all_dis, "all_results_dis.csv", row.names = FALSE)
+colnames(all_dis) <- c(
+  "", "",
+  rep(c("3sources", "BBCSport", "A549", "3cancers"), each = 3)
+)
+write.csv(all_dis, "RealData/all_results_dis.csv", row.names = FALSE)
 
 res_text <- kbl(all_dis,
   booktabs = T, "latex",
@@ -25,24 +28,6 @@ res_text <- kbl(all_dis,
   row.names = FALSE
 )
 df <- kable_styling(res_text)
-sink("all_results_dis.txt")
+sink("RealData/all_results_dis.txt")
 print(df)
 sink()
-
-
-
-
-
-
-
-# read.csv("ResNMTF/ResNMTF/RealData/bbcsport/distance_study.csv")
-# # all <- data.frame("Method" = single_cell$Method, "3Sources" = sources$F.score, "BBCSport" = bbc$F.score, "A549" = single_cell$F.score)
-
-# write.csv(all, "ResNMTF/RealData/all_results_fscores.csv",row.names=FALSE)
-# res_text <- kbl(all,booktabs=T,"latex",escape=FALSE,digits=4,row.names=FALSE)
-# df <- kable_styling(res_text)
-# #save
-# sink("ResNMTF/RealData/all_results_fscores.txt")
-# print(df)
-# sink()
-# read.csv("ResNMTF/ResNMTF/RealData/bbcsport/distance_study.csv")
